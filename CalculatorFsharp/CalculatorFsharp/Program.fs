763e7c4efa18ab83b37430b62c73557ea58ddbfe
Niyaz <nieandevar@mail.ru>
2020-10-03 13:33:53 +0300

    123231

---
+++ CalculatorFsharp/CalculatorFsharp/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let sum (x: float) (y: float): float  =
+    x + y
+    
+let sub (x:float) (y:float) :float =
+    x - y
+    
+let multi (x:float) (y:float) : float =
+    x * y
+
+let div (x:float) (y :float) :float =
+     return let d = x / y
+        
+     Console.ReadKey()|> ignore
+     
+     
